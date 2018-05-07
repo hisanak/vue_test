f316f61fb0d35e43009b2e0c5bbf57575fd8e718
hisanak <terada@asg.cs.tuat.ac.jp>
2018-05-07 12:03:51 +0900

    html/script

---
+++ Gallery.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Gallery</h1>
+    <p>{{txt}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      txt: "hoge",
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ Top.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+    }
+  },
+  mounted() {
+    console.log("Top");
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+
+</style>
+
